2f64a9510dbabe7827ebbddb140d4cbcf1d592a0
omar-etman <omartareketman@gmail.com>
2021-09-11 07:09:22 +0200

    implement details class component

--- adopt-me/src/Components/Details.js
+++ adopt-me/src/Components/Details.js
@@ -1,9 +1,44 @@
-import React from "react"
+import { Component } from "react"
+import { withRouter } from "react-router"
 
-const Details = () => {
-    return (
-        <h2>hi lolololol omg wtf</h2>
-    )
+export class Details extends Component {
+    constructor () {
+        super();
+
+        this.state = {loading: true};
+    }
+
+    async componentDidMount (){
+        const res = await fetch(
+            `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+        )
+        const json = await res.json();
+        this.setState(
+            Object.assign(
+                {
+                    loading: false,
+                    name: json.pets[0].name,
+                },
+                json.pets[0]
+            )
+        );
+    }
+    render() {
+        if(this.state.loading){
+            return <h2>loading...</h2>
+        }
+        const { animal, breed, city, state, description, name} = this.state;
+        return (
+            <div className="details">
+               <h1>{name}</h1>
+               {/* <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2> */}
+
+               <h2>{animal} - {breed} - {city}, {state}</h2>
+               <button>Adopt {name}</button>
+               <p>{description}</p>
+            </div>
+        )
+    }
 }
 
-export default Details
+export default withRouter(Details)
