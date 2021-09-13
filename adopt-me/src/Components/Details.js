43597d8fe0ff98a73ee6ca213446a68f21124a59
omar-etman <omartareketman@gmail.com>
2021-09-13 18:44:01 +0200

    setup the carousel component

--- adopt-me/src/Components/Details.js
+++ adopt-me/src/Components/Details.js
@@ -1,13 +1,11 @@
 import { Component } from "react"
 import { withRouter } from "react-router"
+import Carousel from "./Carousel";
 
 export class Details extends Component {
-    constructor () {
-        super();
-
-        this.state = {loading: true};
-    }
-
+    
+    state = {loading: true};
+    
     async componentDidMount (){
         const res = await fetch(
             `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
@@ -27,15 +25,17 @@ export class Details extends Component {
         if(this.state.loading){
             return <h2>loading...</h2>
         }
-        const { animal, breed, city, state, description, name} = this.state;
+        const { animal, breed, city, state, description, name, images} = this.state;
         return (
             <div className="details">
-               <h1>{name}</h1>
-               {/* <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2> */}
+                <Carousel images={images}/>
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{animal} - {breed} - {city}, {state}</h2>
+                    <button>Adopt {name}</button>
+                    <p>{description}</p>
 
-               <h2>{animal} - {breed} - {city}, {state}</h2>
-               <button>Adopt {name}</button>
-               <p>{description}</p>
+                </div>
             </div>
         )
     }
