8311b12577105940e429fca2ad10fd5c5570578b
omar-etman <omartareketman@gmail.com>
2021-10-05 00:59:04 +0200

    setup tailwid css + apply background img class and header gradient

--- adopt-me/src/Components/App.js
+++ adopt-me/src/Components/App.js
@@ -11,9 +11,16 @@ function App() {
 
   return (
     <ThemeContext.Provider value={theme}>
-      <div>
+      <div
+        className = "p-0 m-0"
+        style ={{  
+          background: "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)"
+        }}
+      >
         <Router>
-          <header>
+          <header
+            className = "w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 to red-500"
+          >
             <Link to="/">
               <h1 id="my-brand">Adopt Me</h1>
             </Link>
