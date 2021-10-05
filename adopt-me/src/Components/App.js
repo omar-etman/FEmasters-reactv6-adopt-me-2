2af6365123180d07cb0638a4aa9e9fab890b3057
omar-etman <omartareketman@gmail.com>
2021-10-05 05:17:11 +0200

    apply codesplitting

--- adopt-me/src/Components/App.js
+++ adopt-me/src/Components/App.js
@@ -1,9 +1,11 @@
 import '../style.css'
-import { useState } from 'react';
-import SearchParams from "./SearchParams";
-import Details from './Details';
+import { useState, lazy, Suspense } from 'react';
+
 import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import ThemeContext from './ThemeContext'
+
+const Details = lazy(()=>import("./Details"));
+const SearchParams = lazy(()=>import("./SearchParams"));
 
 function App() {
   const theme = useState("darkblue");
@@ -12,31 +14,23 @@ function App() {
   return (
     <ThemeContext.Provider value={theme}>
       <div>
-        <Router>
-<<<<<<< HEAD
-<<<<<<< HEAD
-          <header
-            className = "w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 via pink-500 to red-500"
-          >
-=======
-          <header>
->>>>>>> parent of 8311b12 (setup tailwid css + apply background img class and header gradient)
-=======
-          <header>
->>>>>>> parent of 8311b12 (setup tailwid css + apply background img class and header gradient)
-            <Link to="/">
-              <h1 id="my-brand">Adopt Me</h1>
-            </Link>
-          </header>
-          <Switch>
-            <Route exact path = "/">
-              <SearchParams />
-            </Route>
-            <Route path="/details/:id">
-              <Details/>
-            </Route>
-          </Switch>
-        </Router>
+        <Suspense fallback={<h2>loading route ...</h2>}>
+          <Router>
+            <header>
+              <Link to="/">
+                <h1 id="my-brand">Adopt Me</h1>
+              </Link>
+            </header>
+            <Switch>
+              <Route exact path = "/">
+                <SearchParams />
+              </Route>
+              <Route path="/details/:id">
+                <Details/>
+              </Route>
+            </Switch>
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
